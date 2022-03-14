81518aa0d2af440f8f8c541ccb1aa0f5d95ba440
邓江洲 <dengjiangzhouReal@gmail.com>
2022-03-14 11:26:50 +0800

    start

---
+++ RCBacktrace/RCBacktrace.h
@@ -0,0 +1,19 @@
+//
+//  RCBacktrace.h
+//  RCBacktrace
+//
+//  Created by Jz D on 2022/3/14.
+//  Copyright © 2022 roy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RCBacktrace.
+FOUNDATION_EXPORT double RCBacktraceVersionNumber;
+
+//! Project version string for RCBacktrace.
+FOUNDATION_EXPORT const unsigned char RCBacktraceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCBacktrace/PublicHeader.h>
+
+
